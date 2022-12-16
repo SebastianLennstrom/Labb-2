9c43185ba057527c9d20a6cdce8f8b8014545e4a
Sebastian Lennström <sebastian.lennstrom1@gmail.com>
2022-12-16 10:59:15 +0100

    can add tasks to tasks.json

--- Labb2/app.js
+++ Labb2/app.js
@@ -26,8 +26,30 @@ app.get('/task', async (req, res) => {
 
 app.post('/tasks', async (req, res) => {
     try{
+        const task = await fs.readFile('./task.json');
+        res.send(JSON.parse(tasks))
         console.log(req.body)
     }catch(error){
         res.status(500).send({error})}
+})
+app.post("/task", async(req, res) => {
+    try{
+        const task = req.body;
+
+        const listBuffer = await fs.readFile("./tasks.json");
+        const currentTasks = JSON.parse(listBuffer);
+        let taskId = 1;
+        if(currentTasks && currentTasks.length > 0){
+            taskId = currentTasks.reduce(
+                (maxid, currentElement) => (currentElement.id > maxid ? currentElement.id : maxid),
+            taskId);
+        }
+
+        
+        await fs.writeFile("./tasks.json", JSON.stringify([ ...currentTasks, { id: taskId + 1, ...task }]));
+        res.send("Det gick bra");
+    }catch(error){
+        res.status(500).send({error})};
+
 })
 app.listen(PORT, () => console.log('server running on http://localhost:5000'))
